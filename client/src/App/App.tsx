5bba976c07ba6b0986b197a35ed86a4196bf4e61
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-07-02 18:24:54 -0700

    Header component

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import HomePage from '../pages/HomePage/HomePage';
 import ShopPage from '../pages/ShopPage/ShopPage';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop/' component={ShopPage} />
